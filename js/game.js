452dda4a75bfbf1a169bcc9de2d87a2085c5d23a
Artyom Dmitrov <dmitrov.as@phystech.edu>
2025-05-05 21:33:10 +0300

    Add page after new game and update about
    Signed-off-by: Artyom Dmitrov <dmitrov.as@phystech.edu>

---
+++ js/game.js
@@ -0,0 +1,32 @@
+let difficulties = {
+  easy: {
+    name: "Easy",
+    rows: 9,
+    cols: 9,
+    fruits: 10,
+  },
+  medium: {
+    name: "Medium",
+    rows: 16,
+    cols: 16,
+    fruits: 40,
+  },
+  hard: {
+    name: "Hard",
+    rows: 30,
+    cols: 16,
+    fruits: 99,
+  },
+  extreme: {
+    name: "Extreme",
+    rows: 30,
+    cols: 24,
+    fruits: 160,
+  },
+  custom: {
+    name: "Custom",
+    rows: 18, // from 9 to 30
+    cols: 9, // from 9 to 30
+    fruits: 24, // from 0.1 to 0.3 percent by field size
+  },
+};
