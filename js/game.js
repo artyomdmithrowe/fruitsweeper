67ff7e1bab50bf55a9f43468c4b52b9f1ca99511
Artyom Dmitrov <dmitrov.as@phystech.edu>
2025-05-06 15:18:37 +0300

    Add generation field and js-script that begin it
    Signed-off-by: Artyom Dmitrov <dmitrov.as@phystech.edu>

--- js/game.js
+++ js/game.js
@@ -23,10 +23,73 @@ let difficulties = {
     cols: 24,
     fruits: 160,
   },
-  custom: {
-    name: "Custom",
-    rows: 18, // from 9 to 30
-    cols: 9, // from 9 to 30
-    fruits: 24, // from 0.1 to 0.3 percent by field size
-  },
 };
+
+class Cell {
+  constructor() {
+    this.isFruit = false;
+    this.isClicked = false;
+    this.isFlagged = false;
+    this.isFindFruit = false;
+    this.amountOfNeighbours = 0;
+  }
+}
+
+let current_difficult = difficulties.easy;
+let start = false;
+let finish = false;
+let field = [];
+
+let unflaged = current_difficult.fruits;
+
+function setDifficulty(new_difficulty) {
+  current_difficult = new_difficulty;
+  startGame();
+}
+
+function startGame() {
+  createField();
+  setFruits();
+  updateField();
+  unflaged = current_difficult.fruits;
+}
+
+function createField() {
+  for (let i = 0; i < current_difficult.rows + 2; ++i) {
+    field[i] = [];
+    for (let j = 0; j < current_difficult.cols + 2; ++j) {
+      field[i][j] = new Cell();
+    }
+  }
+}
+
+function setFruits() {
+  let fruits = 0;
+  while (fruits < current_difficult.fruits) {
+    let row = 1 + Math.floor(Math.random() * current_difficult.rows);
+    let col = 1 + Math.floor(Math.random() * current_difficult.cols);
+
+    if (!field[row][col].isFruit) {
+      field[row][col].isFruit = true;
+      ++fruits;
+    }
+  }
+}
+
+function updateField() {
+  for (let i = 1; i <= current_difficult.rows; ++i) {
+    for (let j = 1; j <= current_difficult.cols; ++j) {
+      for (let k = -1; k <= 1; ++k) {
+        for (let l = -1; l <= 1; ++l) {
+          if (k == l && l == 0) {
+            continue;
+          }
+
+          if (field[i + k][j + l].isFruit) {
+            ++field[i][j].amountOfNeighbours;
+          }
+        }
+      }
+    }
+  }
+}
