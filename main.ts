0a5c7e29217a38358b7e3fc23daab6eea8cd49ad
Artyom Dmitrov <dmitrov.as@phystech.edu>
2025-05-10 12:31:58 +0300

    open html-page in Deno
    Signed-off-by: Artyom Dmitrov <dmitrov.as@phystech.edu>

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 import { serve } from "https://deno.land/std@0.200.0/http/server.ts";
 
-const handler = (req: Request): Response => {
-  const url = new URL(req.url);
-  return new Response(`Hello from Deno! Path: ${url.pathname}`);
+const handler = async (req: Request): Promise<Response> => {
+  const html = await Deno.readTextFile("./index.html");
+  return new Response(html, {
+    headers: { "Content-Type": "text/html" },
+  });
 };
 
 serve(handler, { port: 8000 });
